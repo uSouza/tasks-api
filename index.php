fb9db4df266e5cfe0337e10e562b69a74ca8e802
Uesley S. Silva <uesleysilva@alunos.utfpr.edu.br>
2019-05-15 00:49:48 -0300

    Implementada toda a lógica de roteamento da API.

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
 <?php
+
 $loader = require __DIR__ . '/vendor/autoload.php';
+
+use TasksApi\Models\Task;
+use TasksApi\Controllers\TasksController;
+
+$app = new \Slim\Slim();
+
+$taskController = new TasksController();
+
+$app->get('/', function() {
+	echo json_encode([
+		"api" => "Tasks Manager",
+		"version" => "1.0.0"
+	]);
+});
+
+$app->get('/tasks(/)', function() use($taskController) {
+  echo json_encode($taskController->getAll());
+});
+
+$app->get('/tasks(/(:id))', function($id = null) use ($taskController) {
+  echo json_encode($taskController->getById($id));
+});
+
+$app->post('/tasks(/)', function() use ($taskController) {
+  $app = \Slim\Slim::getInstance();
+  $json = json_decode($app->request()->getBody());
+  echo json_encode($taskController->insert($json));
+});
+
+$app->put('/tasks(/(:id))', function($id = null) use ($taskController) {
+  $app = \Slim\Slim::getInstance();
+  $json = json_decode($app->request()->getBody());
+  echo json_encode($taskController->update($id, $json));
+});
+
+$app->delete('/tasks(/(:id))', function($id = null) use ($taskController) {
+  echo json_encode($taskController->delete($id));
+});
+
+$app->run();
+
 ?>
